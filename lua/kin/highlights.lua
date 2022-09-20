447cc7c9e4efbf574cb0bf0f813ff3631054eeab
k1nho <kinhong99@gmail.com>
2022-09-20 16:45:39 -0400

    termcolors, cursorline, background

---
+++ lua/kin/highlights.lua
@@ -0,0 +1,6 @@
+vim.opt.cursorline = true     -- Highlight the text line of the cusor
+vim.opt.termguicolors = true  -- Enables 24-Bit RGB colors
+vim.opt.wildoptions = 'pum'   -- Completion matches are show in a popup menu
+vim.opt.winblend = 0          -- Pseudo-transparency for floating window
+vim.opt.pumblend = 5          -- Pseudo-transparency for popup menu
+vim.opt.background = 'dark'   -- Adjust default colors for background
