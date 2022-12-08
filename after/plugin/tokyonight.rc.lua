8fda9d5bd5cc4978469241279ba8a67f162c6ac0
k1nho <kinhong99@gmail.com>
2022-12-08 16:35:35 -0500

    rm

--- after/plugin/tokyonight.rc.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "tokyonight-night"
-local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
-if (not status) then 
-   vim.notify("colorscheme not found")
-   return
-end
