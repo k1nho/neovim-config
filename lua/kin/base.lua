cbfb325502870d61d1d3b097df2a73094d8acbfc
k1nho <kinhong99@gmail.com>
2022-12-16 14:17:48 -0500

    prevent term highlights to persists after search and allow for incremental highlights when searching

--- lua/kin/base.lua
+++ lua/kin/base.lua
@@ -29,7 +29,9 @@ vim.opt.wrap = false                                  -- No wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }      -- allow backspacing over autoindent, over line breaks and over start insert
 vim.opt.path:append { '**' }                          -- Finding files - search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }      -- Ignore node_modules
-vim.opt.mouse = 'a'                                    -- Enable mouse (xD)
+vim.opt.mouse = 'a'                                   -- Enable mouse (xD)
+vim.opt.hlsearch = false                              -- Do not allow terms to be highlighted after search   
+vim.opt.incsearch = true                              -- Allow terms to incrementally be highlighted when searching
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
