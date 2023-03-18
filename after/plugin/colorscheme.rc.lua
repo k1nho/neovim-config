cd6fe19111795341334ca545d2aa2cfaf72ffae8
k1nho <kinhong99@gmail.com>
2023-03-18 12:08:52 -0400

    updt

--- after/plugin/colorscheme.rc.lua
+++ after/plugin/colorscheme.rc.lua
@@ -1,5 +1,5 @@
 local colorscheme = "tokyonight-night"
-local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+local status, _ = pcall(vim.cmd, "colorscheme " ..colorscheme)
 
 if (not status) then
    vim.notify("colorscheme not found")
